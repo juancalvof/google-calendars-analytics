7b04ef9769fc99329bf9dbb1ee68ee2b7fe208d3
juancalvof <juancalvoferrandiz@gmail.com>
2020-09-22 18:14:02 +0200

    Update plotly_graphs.py

--- gca_app.py
+++ gca_app.py
@@ -23,6 +23,7 @@ def df_count_events(list_calendars, type_time_st="BOTH") -> pd.DataFrame:
             filter_items = [x for x in items if type_time in x["end"]]
         elif type_time_st == "BOTH":
             filter_items = items
+
         dict_total_events[x["summary"]] = len(filter_items)
 
     return pd.DataFrame.from_dict(dict_total_events, orient='index', columns=["number of events"])
@@ -46,18 +47,20 @@ def df_events_filter(events, df_events, type_time, format_time, type) -> pd.Data
     # df["recurrence"] = df["id"].map(df_events.set_index('id')["recurrence"])
     df["start"] = df["start"].apply(lambda x: datetime.datetime.strptime(x[type_time], format_time))
     df["end"] = df["end"].apply(lambda x: datetime.datetime.strptime(x[type_time], format_time))
-    df['Week number of the year'] = df['start'].map(lambda x: x.strftime('%Y-%W'))
+    #Test
+    # df['Week number of the year'] = df['start'].map(lambda x: x.strftime('%D'))
+    df['Week number of the year'] = df['start'].map(lambda x: x.strftime('%Y/%W'))
     return df.sort_values(by=['Week number of the year'])
 
 
 @st.cache
-def df_group_sum(df, type, input_calendar_name, dates) -> pd.DataFrame:
+def df_group_sum_weeks(df, type, input_calendar_name, dates) -> pd.DataFrame:
     df[input_calendar_name + " " + type] = df[type]
     df_group = df.groupby("Week number of the year")[input_calendar_name + " " + type].sum()
     df_group = df_group.reset_index().set_index("Week number of the year")
     # Streamlit doesnt take class 'pandas.core.indexes.period.PeriodIndex'
     idx = pd.period_range(dates[0], dates[-1], freq="W")
-    idx = [str(i.strftime('%Y-%W')) for i in idx]
+    idx = [str(i.strftime('%Y/%W')) for i in idx]
     df_group = df_group.reindex(idx, fill_value=pd.Timedelta('0 days'))
     if type == "hours":
         df_group[input_calendar_name + " " + type] = (df_group[input_calendar_name + " " + type].dt.seconds / 3600)
@@ -65,7 +68,6 @@ def df_group_sum(df, type, input_calendar_name, dates) -> pd.DataFrame:
         df_group[input_calendar_name + " " + type] = df_group[input_calendar_name + " " + type].dt.days
 
     return df_group
-
 
 def specific_analysis(df_events, input_calendar_events, input_calendar_name, type_time, format_time, type, dates):
     df = df_events_filter(input_calendar_events, df_events, type_time, format_time, type)
@@ -85,13 +87,15 @@ def specific_analysis(df_events, input_calendar_events, input_calendar_name, typ
     if len(df.index) == 0:
         st.write("No values in this type.")
     else:
-        df_group = df_group_sum(df, type, input_calendar_name, dates)
+        df_group = df_group_sum_weeks(df, type, input_calendar_name, dates)
         st.dataframe(df_group)
-        st.line_chart(df_group)
+        st.plotly_chart(pg.line_chart(df_group, df_group.index, df_group.columns[0]), use_container_width=True,
+                        config=pg.UNBRAND_CONFIG)
 
 
 def comparative_analysis(list_calendars_selected, type_time, format_time, type, dates):
-    idx = pd.period_range(dates[0], dates[-1], freq="M").astype("str")
+    idx = pd.period_range(dates[0], dates[-1], freq="W")
+    idx = [str(i.strftime('%Y/%W')) for i in idx]
     df_final = pd.DataFrame(index=idx)
     if len(list_calendars_selected) == 0:
         st.write("No calendar selected.")
@@ -105,11 +109,14 @@ def comparative_analysis(list_calendars_selected, type_time, format_time, type, 
             else:
                 df_events = pd.DataFrame(input_calendar_events)
                 df = df_events_filter(input_calendar_events, df_events, type_time, format_time, type)
-                df_group = df_group_sum(df, type, calendar, dates)
+                df_group = df_group_sum_weeks(df, type, calendar, dates)
                 df_final = df_final.join(df_group)
 
         st.dataframe(df_final)
-        st.line_chart(df_final)
+        st.plotly_chart(pg.line_chart_multiple(df_final),
+                        use_container_width=True,
+                        config=pg.UNBRAND_CONFIG)
+
 
 
 if __name__ == "__main__":
@@ -134,8 +141,8 @@ if __name__ == "__main__":
     st.write("### **1_b** Number of events in each calendar")
     df_total_events = df_count_events(list_calendar, input_calendar_general_type)
     st.dataframe(df_total_events)
-    st.bar_chart(df_total_events, width=100, height=400, use_container_width=True)
-    st.plotly_chart(pg.test_bar_chart(), use_container_width=True)
+    st.plotly_chart(pg.bar_chart(df_total_events, df_total_events.index, 'number of events'), use_container_width=True,
+                        config=pg.UNBRAND_CONFIG)
 
     # 2 Specific analysis
     list_calendar_names = [x["summary"] for x in list_calendar]
--- plotly_graphs.py
+++ plotly_graphs.py
@@ -1,12 +1,62 @@
 import plotly.express as px
+import plotly.graph_objects as go
 
+UNBRAND_CONFIG = dict(modeBarButtonsToRemove=['sendDataToCloud', 'toggleSpikelines', 'hoverCompareCartesian'],
+                      displaylogo=False, showLink=False,displayModeBar=False, showTips=False)
 
-def test_bar_chart():
+def test_line_chart():
     df = px.data.gapminder().query("continent=='Oceania'")
     fig = px.line(df, x="year", y="lifeExp", color='country')
     return fig
 
-def bar_chart():
-    df = px.data.gapminder().query("continent=='Oceania'")
-    fig = px.line(df, x="year", y="lifeExp", color='country')
+
+def line_chart(df, x, y):
+    fig = px.line(df, x=x, y=y, template="simple_white")
+    fig.update_xaxes(
+        tickangle=270,
+        dtick="M1",
+        tickformat="%Y",
+        ticklabelmode="period",
+        showspikes=True,
+        spikethickness=1,
+        fixedrange=True)
+    fig.update_yaxes(showspikes=True, fixedrange=True, spikethickness=1)
+    fig.update_traces(marker_color='rgb(0,255,194)', line_color='rgb(0,255,194)')
+    fig.update_layout(xaxis_title='YEAR AND WEEK NUMBER OF THE YEAR',
+                      yaxis_title='HOURS')
+    fig.update_traces(mode="markers+lines", hovertemplate=None)
+    fig.update_layout(hovermode="x unified")
+    return fig
+
+
+def line_chart_multiple(df):
+    fig = go.Figure()
+    for column in df.columns:
+        fig.add_trace(go.Scatter(x=df.index, y=df[column], name=column))
+    fig.update_xaxes(
+        tickangle=270,
+        dtick="M1",
+        tickformat="%b\n%Y",
+        ticklabelmode="period",
+        showspikes=True,
+        spikethickness=1,
+        fixedrange=True)
+    fig.update_yaxes(showspikes=True, fixedrange=True, spikethickness=1)
+    fig.update_layout(template="simple_white")
+    fig.update_layout(xaxis_title='YEAR AND WEEK NUMBER OF THE YEAR',
+                      yaxis_title='HOURS')
+    fig.update_traces(mode="markers+lines", hovertemplate=None)
+    fig.update_layout(hovermode="x unified")
+    # fig.update_traces(marker_color='rgb(200,210,240)', marker_line_color='rgb(8,48,107)',
+    #                   marker_line_width=1.5, line_color='rgb(0,255,194)')
+    return fig
+
+def bar_chart(df, x, y):
+    fig = px.bar(df, x=x, y=y, template="simple_white",
+                 labels={'number of events': 'NUMBER OF EVENTS PER CALENDAR', 'index': 'CALENDAR NAME'})
+    fig.update_xaxes(showspikes=True, fixedrange=True, spikethickness=1)
+    fig.update_yaxes(showspikes=True, fixedrange=True, spikethickness=1)
+    fig.update_layout(hovermode='x')
+    fig.update_traces(marker_color='rgb(0,255,194)', marker_line_color='rgb(8,48,107)',
+                      marker_line_width=1.5)
     return fig
